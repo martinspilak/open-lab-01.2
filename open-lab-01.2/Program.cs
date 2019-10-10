1fd93d962f0f45721cd69f65e4beb0f7c2c0dbbe
martinspilak <martinecko24@gmail.com>
2019-10-10 09:12:41 +0200

    oprava

--- open-lab-01.2/Program.cs
+++ open-lab-01.2/Program.cs
@@ -4,12 +4,16 @@ namespace open_lab_01._2
 {
     class Program
     {
+        public static int sum(int a, int b)
+        {
+            return a + b;
+        }
         static void Main(string[] args)
         {
             int a = Convert.ToInt32(Console.ReadLine());
             int b = Convert.ToInt32(Console.ReadLine());
-            int c = a + b;
-            Console.WriteLine(c);
+         
+          Console.WriteLine(sum(a, b));
         }
     }
 }
